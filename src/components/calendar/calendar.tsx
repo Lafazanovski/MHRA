a627e8e2c2069c133da9b5c821c654c66360ce27
Kristijan Lafazanovski <lafazanovski.kristijan1993@gmail.com>
2026-04-18 16:28:39 +0200

    update calendar

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -1,6 +1,6 @@
 const Calendar = () => {
   const days = [null,null,null,null,null,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
-  const eventDays = [8, 15, 22, 24]; // ← датуми со настани
+  const eventDays = [8, 15, 22, 24]; 
 
   const weeks: (number | null)[][] = [];
   let week: (number | null)[] = [];
